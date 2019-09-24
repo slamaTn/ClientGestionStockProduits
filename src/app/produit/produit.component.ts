9b0083b0802f2d578967c6ff738e63c7d24db2cc
slama.tn <slama.haythem.1990@gmail.com>
2019-09-24 16:51:15 +0100

    ajouter id as primary key

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -67,7 +67,7 @@ export class ProduitComponent implements OnInit{
   }
 
   deleteProduit(){
-    this.produitService.deleteProduit(this.selectedProduit.ref).subscribe(
+    this.produitService.deleteProduit(this.selectedProduit.id).subscribe(
       res => {
         this.selectedProduit = new Produit();
         this.loadProduits();
